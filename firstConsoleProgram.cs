303dd82c6cd3a3097d02444edfbc9ec799ef37ef
Ömer Mert <76480058+Mr-OMD@users.noreply.github.com>
2021-11-04 14:55:04 +0300

    Add files via upload

---
+++ firstConsoleProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Console_Programming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adı: ");
+            string name=Console.ReadLine();
+            Console.WriteLine("Soyadı: ");
+            string surname=Console.ReadLine();
+
+            Console.WriteLine("hello detective " + name + " " + surname);
+        }
+        
+    }
+}
